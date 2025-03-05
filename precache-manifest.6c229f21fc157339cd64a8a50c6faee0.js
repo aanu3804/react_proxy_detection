c48bb9fee5d26f7ade1334110b06c7dc6ce3a042
Anand <anandsai2004@gmail.com>
2025-03-05 17:42:02 +0530

    Updates

---
+++ precache-manifest.6c229f21fc157339cd64a8a50c6faee0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6699ac30d44fb2b9776d5bdb46726e6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "653f6e59fa217eb2a8b4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "87448be9eb2765b72e22",
+    "url": "/static/js/2.a5f0a4db.chunk.js"
+  },
+  {
+    "revision": "5099652deffd979d970e",
+    "url": "/static/js/3.d5f86460.chunk.js"
+  },
+  {
+    "revision": "653f6e59fa217eb2a8b4",
+    "url": "/static/js/main.e28a60c0.chunk.js"
+  },
+  {
+    "revision": "b3d99bc629c5a9184875",
+    "url": "/static/js/runtime~main.2091b4db.js"
+  }
+]);
